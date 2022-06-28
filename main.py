bc07f83c3a3ac094f22793bf9ee4c80a5699aeb4
Fear <89424149+Fear-MK@users.noreply.github.com>
2022-06-29 00:01:49 +0100

    Added ^mmrlr, ^lr and ^mmr.

--- main.py
+++ main.py
@@ -10,24 +10,62 @@ with open("private.txt", "r") as f:
     token=f.read()[8:]
 
 @bot.command()
-async def mmr(ctx):
-    description=""
-    args=[text.strip() for text in ctx.message.content[5:].split(",")]
-    json_data=api_request(ctx, args)
-    if not json_data:
+async def mmr(ctx, *args):
+    json_data, ladder_type=api_request(ctx, split_args(list(args)))
+    if json_data == "Failed":
         return
-    for player in json_data["results"]:
-        description+=f" []"
-
-    embedVar = discord.Embed(title="",description=,colour=discord.Color.blue())
-    embedVar.set_author(
-        name='Current MMR',
-        icon_url='https://www.mkwlounge.gg/images/logo.png'
+  
+    embed = discord.Embed(colour=discord.Color.purple())
+    embed.set_author(
+        name=f"Lounge {ladder_type.upper()} MMR",
+        icon_url="https://cdn.discordapp.com/icons/387347467332485122/5035e39312f7aadd75a29011e37a96f1.png"
     )
-        
+
+    json_data.sort(key=lambda x: x["current_mmr"], reverse=True)
+
+    for player in json_data:
+        embed.add_field(name=player["player_name"],value=f"[{str(player['current_mmr'])}]({str(player['url'])})",inline=True)
+    
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def test(ctx):
-    print(ctx.author.nick)
+async def lr(ctx, *args):
+    json_data, ladder_type=api_request(ctx, split_args(list(args)))
+    if json_data == "Failed":
+        return
+  
+    embed = discord.Embed(colour=discord.Color.purple())
+    embed.set_author(
+        name=f"Lounge {ladder_type.upper()} LR",
+        icon_url="https://cdn.discordapp.com/icons/387347467332485122/5035e39312f7aadd75a29011e37a96f1.png"
+    )
+
+    json_data.sort(key=lambda x: x["current_lr"], reverse=True)
+
+    for player in json_data:
+        embed.add_field(name=player["player_name"],value=f"[{str(player['current_lr'])}]({str(player['url'])})",inline=True)
+    
+    await ctx.send(embed=embed)
+
+@bot.command(aliases=["lrmmr"])
+async def mmrlr(ctx, *args):
+    json_data, ladder_type=api_request(ctx, split_args(list(args)))
+    if json_data == "Failed":
+        return
+  
+    embed = discord.Embed(colour=discord.Color.purple())
+    embed.set_author(
+        name=f"Lounge {ladder_type.upper()} LR/MMR",
+        icon_url="https://cdn.discordapp.com/icons/387347467332485122/5035e39312f7aadd75a29011e37a96f1.png"
+    )
+
+    json_data.sort(key=lambda x: x["current_lr"], reverse=True)
+
+    for player in json_data:
+        embed.add_field(name=player["player_name"],value=f"[{str(player['current_lr'])} LR/{str(player['current_mmr'])} MMR]({str(player['url'])})",inline=True)
+    
+    await ctx.send(embed=embed)
+
+print("Bot Started")
 
 bot.run(token)
--- shared.py
+++ shared.py
@@ -2,42 +2,41 @@ import requests
 import json
 from common import *
 
+def split_args(args):
+    for x in range(len(args)):
+        args[x:x+1]=args[x].split(",")
+    args = list(filter(None, args))
+    return args
+
 def api_request(ctx, args):
-    name=[ctx.author.nick]
-
-    if len(args) >= 2:
-        if args[0].lower() in ladder_id:
-            name, ladder=args[1:], ladder_id[args[0].lower()]
+    print(ctx.message.author.nick)
+    if len(args) == 0:
+        name=[ctx.author.display_name]
+        if ctx.channel.name[:2] in ["rt", "ct"]:
+            ladder = ctx.channel.name[:2]
         else:
-            name=args
-            if ctx.channel.name[:2] in ladder_id:
-                ladder = ladder_id[ctx.channel.name[:2]]
-            else:
-                ladder = ladder_id["rt"]
-    elif len(args) == 1:
-        if args[0].lower() in ladder_id and len(args) == 1:
-            ladder = ladder_id[args[0].lower()]
+            ladder = "rt"
+    elif args[0].lower() in ["rt","ct"]:
+        ladder = args[0].lower()
+        if len(args) == 1:
+            name=[ctx.author.display_name]
         else:
-            if ctx.channel.name[:2] in ladder_id:
-                ladder = ladder_id[ctx.channel.name[:2]]
-            else:
-                ladder = ladder_id["rt"]
-    elif len(args) == 0:
-        ladder=ladder_id["rt"]
-
-    if not isinstance(name, list):
-        url=f"https://www.mkwlounge.gg/api/ladderplayer.php?player_name={name}&ladder_id={ladder}"
+            name = args[1:]
     else:
-        url=f"https://www.mkwlounge.gg/api/ladderplayer.php?player_names={',%20'.join(name)}&ladder_id={ladder}"
+        name = args
+        if ctx.channel.name[:2] in ["rt", "ct"]:
+            ladder = ctx.channel.name[:2]
+        else:
+            ladder = "rt"
 
-    print(url)
+    url=f"https://www.mkwlounge.gg/api/ladderplayer.php?player_names={',%20'.join(name)}&ladder_id={ladder_id[ladder]}"
 
     data=requests.get(url)
 
     try:
         if len(json.loads(str(data.text))["results"]) == 0:
-            return False
+            return "Failed", "Failed"
     except KeyError:
-        return False
+        return "Failed", "Failed"
 
-    return json.loads(data.text)
+    return json.loads(data.text)["results"], ladder
